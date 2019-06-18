4f53f0feed404871966a6e9a118bb215cffdda5e
guivo <33522004+guivo@users.noreply.github.com>
2019-06-18 23:32:28 +0200

    First commit

---
+++ R/twitterwidget.R
@@ -0,0 +1,52 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+twitterwidget <- function(twid, width = NULL, height = NULL, elementId = NULL) {
+
+  # forward options using x
+  x = list(
+    twid = twid
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'twitterwidget',
+    x,
+    width = width,
+    height = height,
+    package = 'twitterwidget',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for twitterwidget
+#'
+#' Output and render functions for using twitterwidget within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a twitterwidget
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name twitterwidget-shiny
+#'
+#' @export
+twitterwidgetOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'twitterwidget', width, height, package = 'twitterwidget')
+}
+
+#' @rdname twitterwidget-shiny
+#' @export
+renderTwitterwidget <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, twitterwidgetOutput, env, quoted = TRUE)
+}
---
+++ inst/htmlwidgets/twitterwidget.js
@@ -0,0 +1,48 @@
+HTMLWidgets.widget({
+
+  name: 'twitterwidget',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    window.twttr = (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0],
+        t = window.twttr || {};
+      if (d.getElementById(id)) return t;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://platform.twitter.com/widgets.js";
+      fjs.parentNode.insertBefore(js, fjs);
+
+      t._e = [];
+      t.ready = function(f) {
+        t._e.push(f);
+      };
+
+      return t;
+    }(document, "script", "twitter-wjs"));
+
+    return {
+
+      renderValue: function(x) {
+
+          twttr.widgets.createTweet(
+            x.twid,
+            document.getElementById(el.id),
+            {
+              theme: 'dark'
+            }
+          );
+
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
