7f176b2eaca7bc7417bf041d04821f2da70239bc
guivo <33522004+guivo@users.noreply.github.com>
2019-06-20 15:34:31 +0200

    Clean the script

--- inst/htmlwidgets/twitterwidget.js
+++ inst/htmlwidgets/twitterwidget.js
@@ -5,24 +5,6 @@ HTMLWidgets.widget({
   type: 'output',
 
   factory: function(el, width, height) {
-    /* TODO: detail comparison
-    window.twttr = (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0],
-        t = window.twttr || {};
-      if (d.getElementById(id)) return t;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://platform.twitter.com/widgets.js";
-      fjs.parentNode.insertBefore(js, fjs);
-
-      t._e = [];
-      t.ready = function(f) {
-        t._e.push(f);
-      };
-
-      return t;
-    }(document, "script", "twitter-wjs"));
-    */
 
     return {
 
